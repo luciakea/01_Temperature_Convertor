8f9ccaf54ec6266803e8e0ceed9f4b2471e8426d
luciakea <101675436+luciakea@users.noreply.github.com>
2022-04-07 19:35:28 +1200

    Version 1

---
+++ 05_Rounding_v1.py
@@ -0,0 +1,12 @@
+#Display output using int / float
+
+
+to_round = [1/1, 1/2, 1/3]
+print("***** Numbers to round *****")
+print(to_round)
+
+print()
+print("***** Rounded numbers *****")
+
+for items in to_round:
+    print(int(items))
